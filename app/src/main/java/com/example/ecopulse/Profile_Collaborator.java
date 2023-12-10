c905450547c088ffcbeb9b501f91a85d667fa26d
leezh <22004832@siswa.um.edu.my>
2023-12-10 14:37:23 +0800

    Profile_Collaborator add ManagePosts button

--- app/src/main/java/com/example/ecopulse/Profile_Collaborator.java
+++ app/src/main/java/com/example/ecopulse/Profile_Collaborator.java
@@ -2,57 +2,23 @@ package com.example.ecopulse;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.*;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Profile_Collaborator#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Profile_Collaborator extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    Button BtnManagePosts;
 
     public Profile_Collaborator() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Profile_Collaborator.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Profile_Collaborator newInstance(String param1, String param2) {
-        Profile_Collaborator fragment = new Profile_Collaborator();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -60,5 +26,25 @@ public class Profile_Collaborator extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile__collaborator, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        BtnManagePosts = view.findViewById(R.id.BtnManagePosts);
+        /*  navigate to ManagePostsFragment
+        BtnManagePosts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment ManagePostsFragment = new ManagePostsFragment();
+                FragmentManager manager = getFragmentManager();
+                FragmentTransaction transaction = manager.beginTransaction();
+
+                transaction.replace(R.id.profileLinearLayout,ManagePostsFragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        }); */
     }
 }
