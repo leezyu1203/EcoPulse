58660585a9ad6d9380d16deac0603b700dcf108d
22004743 <22004743@siswa.um.edu.my>
2024-01-13 00:32:48 +0800

    WasteExampleListFragment.java updated

--- app/src/main/java/com/example/ecopulse/WasteExampleListFragment.java
+++ app/src/main/java/com/example/ecopulse/WasteExampleListFragment.java
@@ -28,39 +28,44 @@ public class WasteExampleListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_waste_example_list, container, false);
 
-        wasteTypeTitle =rootView.findViewById (R.id.wasteTypeTitle);
 
-        guidanceRecWasteList = rootView.findViewById(R.id.guidanceRecWasteList);
+        wasteTypeTitle =rootView.findViewById (R.id.wasteTypeTitle);//page title
+        guidanceRecWasteList = rootView.findViewById(R.id.guidanceRecWasteList);//recycler view for waste example list
+
+        //adapter for recycler view
         wasteList = new ArrayList<>();
         wasteGuidanceAdapter =  new WasteGuidanceAdapter(requireContext(), wasteList);
         guidanceRecWasteList.setLayoutManager(new LinearLayoutManager(requireContext()));
         guidanceRecWasteList.setAdapter(wasteGuidanceAdapter);
 
+        //check which waste type example list page is navigated to
         int wasteType = requireArguments().getInt("wasteType", -1);
+
+        //database connection
         guidanceDatabaseReference = FirebaseFirestore.getInstance();
 
-        if(wasteType == 0)
+        if(wasteType == 0)//recyclable waste
         {
-            wasteList.clear();
+            wasteList.clear();// clear the previous result
             wasteTypeTitle.setText("Recyclable Waste");
             showWasteList(0);
 
         }
-        if(wasteType == 1)
+        if(wasteType == 1)//hazardous waste
         {
             wasteList.clear();
             wasteTypeTitle.setText("Hazardous Waste");
             showWasteList(1);
 
         }
-        if(wasteType == 2)
+        if(wasteType == 2)//household food waste
         {
             wasteList.clear();
             wasteTypeTitle.setText("Household Food Waste");
             showWasteList(2);
 
         }
-        if(wasteType == 3)
+        if(wasteType == 3)//residual waste
         {
             wasteList.clear();
             wasteTypeTitle.setText("Residual Waste");
@@ -71,6 +76,8 @@ public class WasteExampleListFragment extends Fragment {
         return rootView;
     }
 
+
+    //connected to the database, fetch the data from entity of recycling_guidance
     public void showWasteList(int wasteType)
     {
         guidanceDatabaseReference.collection("recycling_guidance")
@@ -80,11 +87,9 @@ public class WasteExampleListFragment extends Fragment {
                     for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
                         String wasteName = documentSnapshot.getString("waste_name");
                         String wasteDesc = documentSnapshot.getString("waste_desc");
-
                         String result = wasteName + " _ " + "" + " _ " + wasteDesc;
                         wasteList.add(result);
                     }
-
                     wasteGuidanceAdapter.notifyDataSetChanged();
                 })
                 .addOnFailureListener(e ->{
